6143c4f15a0d17060085215c9cf157447fa2aeee
mohammadquanit7 <mohammadquanit7@gmail.com>
2026-05-27 01:03:37 +0530

    Create Dorado-ff-Info.js

---
+++ api/Dorado-ff-Info.js
@@ -0,0 +1,32 @@
+module.exports = async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  try {
+    const { uid, region } = req.query;
+
+    if (!uid || !region) {
+      return res.status(400).json({
+        status: false,
+        message: "uid and region required",
+        powered_by: "@Prime_x_Samiul"
+      });
+    }
+
+    const url = `https://flexdevloper-bot-ff-info-api.vercel.app/player-info?region=${region}&uid=${uid}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    return res.status(200).json({
+      ...data,
+      powered_by: "@Prime_x_Samiul"
+    });
+
+  } catch (e) {
+    return res.status(500).json({
+      status: false,
+      error: e.message,
+      powered_by: "@Prime_x_Samiul"
+    });
+  }
+};
